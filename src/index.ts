4ca53068bcef14aa2ed5537493e340bad92c7e2f
olenaweb <olena.nevzorova@gmail.com>
2024-10-21 02:18:21 +0300

    fix: Fixed configuration error

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ import { createServer, IncomingMessage, ServerResponse } from "http";
 const host = "localhost";
 export const port = parseInt(process.env.PORT!) || 3500;
 
-export const Server = createServer(
+export const server = createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log('"req="', req);
     console.log('"res="', res);
   },
 );
 
-Server.listen(port, () => {
-  process.stdout.write(`Server is running on http://${host}:${port}${EOL}`);
+server.listen(port).on("listening", () => {
+  console.log(`Server HTTP is listening on http://${host}:${port}${EOL}`);
 });
